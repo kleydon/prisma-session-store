3ddc2d7462383108fd503e3ee39f4dc0cd3ed697
William Sedlacek <wsedlacekc@gmail.com>
2020-09-05 10:34:39 -0700

    feat(): External Logger
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ src/lib/logger.ts
@@ -0,0 +1,25 @@
+import { ILevel, ILogger } from '../@types/logger';
+
+export class Logger {
+  constructor(
+    private readonly logger: ILogger | false,
+    private readonly level: ILevel | ILevel[]
+  ) {}
+
+  private checkLevel(level: ILevel): boolean {
+    if (Array.isArray(this.level)) return this.level.includes(level);
+    else return this.level === level;
+  }
+
+  public log(message: string): void {
+    if (this.logger && this.checkLevel('log')) this.logger.log(message);
+  }
+
+  public warn(message: string): void {
+    if (this.logger && this.checkLevel('warn')) this.logger.warn(message);
+  }
+
+  public error(error: unknown): void {
+    if (this.logger && this.checkLevel('error')) this.logger.error(error);
+  }
+}
