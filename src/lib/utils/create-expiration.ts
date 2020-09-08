f70e75f8c1427165f1bb399bda5086db2ab81c14
William Sedlacek <wsedlacekc@gmail.com>
2020-09-08 10:20:36 -0700

    doc(): Document `createExpiration()` rounding
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/lib/utils/create-expiration.ts
+++ src/lib/utils/create-expiration.ts
@@ -1,5 +1,9 @@
 /**
  * Creates a `Date` object that is a certain number of milliseconds in the future.
+ * Note: The time will be rounded down to the nearest `0.01s` to enure expirations
+ * created at roughly the same time match. This is required in order to test these
+ * values.
+ *
  * @param milliseconds the number of milliseconds before the expiration date
  */
 export const createExpiration = (shelfLifeMs: number) =>
