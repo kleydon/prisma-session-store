df5a3e753ba8888e889d37c14f5bf6600d28951d
William Sedlacek <wsedlacekc@gmail.com>
2020-09-07 21:18:03 -0700

    style(): Improve parameter name
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/lib/utils/create-expiration.ts
+++ src/lib/utils/create-expiration.ts
@@ -2,5 +2,5 @@
  * Creates a `Date` object that is a certain number of milliseconds in the future.
  * @param milliseconds the number of milliseconds before the expiration date
  */
-export const createExpiration = (milliseconds: number) =>
-  new Date(Math.floor(new Date().valueOf() / 10) * 10 + milliseconds);
+export const createExpiration = (shelfLifeMs: number) =>
+  new Date(Math.floor(new Date().valueOf() / 10) * 10 + shelfLifeMs);
