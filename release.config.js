e78accf7c302e08e15801bc54bcf894b565cd63b
William Sedlacek <wsedlacekc@gmail.com>
2020-09-11 10:57:12 -0700

    feat: setup CI/CD
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  preset: 'angular',
+};
