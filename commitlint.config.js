ff3bd79c65751db64e504a69d1c8b4815fdb5a03
kleydon <kleydon@users.noreply.github.com>
2021-12-06 11:31:30 -0800

    fix: change commit lint overrides to warnings

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 200],
-    'footer-max-line-length': [2, 'always', 200],
+    'body-max-line-length': [1, 'always', 200],
+    'footer-max-line-length': [1, 'always', 200],
   },
 };
