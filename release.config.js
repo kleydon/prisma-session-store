79a0860ba6f49560e6de0ddf61d14b1875abac06
kleydon <kleydon@users.noreply.github.com>
2025-12-27 11:17:17 -0700

    chore: release file cleanup
    * Cleaned up release.config.js
    * build.yml

--- release.config.js
+++ release.config.js
@@ -1,27 +1,3 @@
-// module.exports = {
-//   plugins: [
-//     '@semantic-release/commit-analyzer',
-//     '@semantic-release/release-notes-generator',
-//     [
-//       '@semantic-release/changelog',
-//       {
-//         changelogFile: 'CHANGELOG.md',
-//       },
-//     ],
-//     '@semantic-release/npm',
-//     '@semantic-release/github',
-//     [
-//       '@semantic-release/git',
-//       {
-//         assets: ['package.json', 'CHANGELOG.md'],
-//         message:
-//           'chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-//       },
-//     ],
-//   ],
-//   preset: 'angular',
-// };
-
 module.exports = {
   branches: ['master'], // release from master branch
   plugins: [
@@ -30,5 +6,4 @@ module.exports = {
     '@semantic-release/npm',
     '@semantic-release/github',
   ],
-  //preset: 'angular',
 };
