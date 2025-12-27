6c4940f60095e5eedd579d14e68529710ad3e2fa
kleydon <kleydon@users.noreply.github.com>
2025-12-27 11:09:17 -0700

    chore: updating semantic release process
    * Removing change log, as keeping it results in unnecessary complexity

--- release.config.js
+++ release.config.js
@@ -1,23 +1,34 @@
+// module.exports = {
+//   plugins: [
+//     '@semantic-release/commit-analyzer',
+//     '@semantic-release/release-notes-generator',
+//     [
+//       '@semantic-release/changelog',
+//       {
+//         changelogFile: 'CHANGELOG.md',
+//       },
+//     ],
+//     '@semantic-release/npm',
+//     '@semantic-release/github',
+//     [
+//       '@semantic-release/git',
+//       {
+//         assets: ['package.json', 'CHANGELOG.md'],
+//         message:
+//           'chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+//       },
+//     ],
+//   ],
+//   preset: 'angular',
+// };
+
 module.exports = {
+  branches: ['master'], // release from master branch
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
     '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message:
-          'chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
   ],
-  preset: 'angular',
+  //preset: 'angular',
 };
